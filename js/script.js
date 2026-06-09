3e2db10a49c0524d031df83c088d7fa52a00cbf3
Sumit Jhotwal <jhotwalsumit@gmail.com>
2026-06-09 15:53:23 +0530

    Initial portfolio website

---
+++ js/script.js
@@ -0,0 +1,130 @@
+const roles = [
+  "Web Developer",
+  "Software Tester",
+  "Frontend Developer"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let currentText = "";
+let isDeleting = false;
+
+function typeEffect() {
+
+  const typing = document.getElementById("typing");
+
+  currentText = roles[roleIndex];
+
+  if (!isDeleting) {
+    typing.textContent = currentText.substring(0, charIndex + 1);
+    charIndex++;
+
+    if (charIndex === currentText.length) {
+      isDeleting = true;
+      setTimeout(typeEffect, 1500);
+      return;
+    }
+
+  } else {
+
+    typing.textContent = currentText.substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      roleIndex = (roleIndex + 1) % roles.length;
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop - 150;
+
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+
+  });
+
+});
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+  const updateCounter = () => {
+
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const increment = target / 50;
+
+    if (count < target) {
+
+      counter.innerText =
+      Math.ceil(count + increment);
+
+      setTimeout(updateCounter, 30);
+
+    } else {
+
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+
+});
+const themeBtn =
+document.getElementById("theme-btn");
+
+themeBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("light-mode");
+
+});
+const topBtn =
+document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 300){
+
+    topBtn.style.display = "block";
+
+  }else{
+
+    topBtn.style.display = "none";
+
+  }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+  window.scrollTo({
+    top:0,
+    behavior:"smooth"
+  });
+
+});
